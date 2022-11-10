6994b5d1e94f489d8103c955ec29b062ba72fc3f
dqarias <dino.quispea@gmail.com>
2022-11-10 11:24:54 -0500

    Add styling for hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,34 @@
 import React from 'react';
+import TypeWriterEffect from 'react-typewriter-effect';
 
 const Hero = () => (
-  <section>
-    <h2>Hi, my name is</h2>
-    <h2>Dino</h2>
-    <h2>I build things for the web</h2>
-    <p>
+  <section className="hero">
+    <h2 className="hero__title-1">Hi, my name is</h2>
+    <h2 className="hero__title-2">Dino Quispe</h2>
+    <TypeWriterEffect
+      textStyle={{
+        fontFamily: 'Montserrat',
+        color: '#f0d2d2',
+        fontWeight: 700,
+        fontSize: '6rem',
+      }}
+      startDelay={0}
+      cursorColor="#f0d2d2"
+      multiText={[
+        'I build things for the web',
+        'Full Stack Developer',
+      ]}
+      multiTextDelay={3000}
+      typeSpeed={70}
+      multiTextLoop
+    />
+    <p className="hero__description">
       I can help you build a product ,
       feature or website Look through some of my work and experience! If
       you like what you see and have a project you need coded, don’t
       hestiate to contact me.
     </p>
+    <button className="btn" type="button">LET’S CONNECT</button>
   </section>
 );
 
