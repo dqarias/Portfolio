2aa9ec86d61054accc363412deb1091749ed4c8e
dqarias <dino.quispea@gmail.com>
2022-11-16 07:10:22 -0500

    Update theme on hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,63 +1,133 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import TypeWriterEffect from 'react-typewriter-effect';
 import { motion } from 'framer-motion';
+import ThemeContext from '../context/theme/ThemeContext';
 
-const Hero = () => (
-  <motion.section
-    className="hero"
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    transition={{ delay: 0.5, duration: 0.5 }}
-  >
-    <h2 className="hero__title-1">Hi, my name is</h2>
-    <h2 className="hero__title-2">Dino Quispe</h2>
-    <div className="hero__title-3">
-      <TypeWriterEffect
-        textStyle={{
-          fontFamily: 'Montserrat',
-          color: '#f0d2d2',
-          fontWeight: 700,
-          fontSize: '6rem',
-        }}
-        startDelay={0}
-        cursorColor="#f0d2d2"
-        multiText={[
-          'I build things for the web',
-          'Full Stack Developer',
-        ]}
-        multiTextDelay={3000}
-        typeSpeed={70}
-        multiTextLoop
-      />
-    </div>
-    <div className="hero__title-4">
-      <TypeWriterEffect
-        textStyle={{
-          fontFamily: 'Montserrat',
-          color: '#f0d2d2',
-          fontWeight: 700,
-          fontSize: '3.2rem',
-        }}
-        startDelay={0}
-        cursorColor="#f0d2d2"
-        multiText={[
-          'I build things for the web',
-          'Full Stack Developer',
-        ]}
-        multiTextDelay={3000}
-        typeSpeed={70}
-        multiTextLoop
-      />
-    </div>
+const Hero = () => {
+  const { darkMode } = useContext(ThemeContext);
 
-    <p className="hero__description">
-      I can help you build a product ,
-      feature or website Look through some of my work and experience! If
-      you like what you see and have a project you need coded, don’t
-      hestiate to contact me.
-    </p>
-    <button className="btn" type="button">LET’S CONNECT</button>
-  </motion.section>
-);
+  return (
+    <motion.section
+      className={
+      darkMode
+        ? 'hero'
+        : 'hero hero-light'
+    }
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.5, duration: 0.5 }}
+    >
+      <h2 className="hero__title-1">Hi, my name is</h2>
+      <h2
+        className={
+        darkMode
+          ? 'hero__title-2'
+          : 'hero__title-2 hero__title-2-light'
+      }
+      >
+        Dino Quispe
+      </h2>
+      {darkMode
+        ? (
+          <div>
+            <div className="hero__title-3">
+              <TypeWriterEffect
+                textStyle={{
+                  fontFamily: 'Montserrat',
+                  color: '#f0d2d2',
+                  fontWeight: 700,
+                  fontSize: '6rem',
+                }}
+                startDelay={0}
+                cursorColor="#f0d2d2"
+                multiText={[
+                  'I build things for the web',
+                  'Full Stack Developer',
+                ]}
+                multiTextDelay={3000}
+                typeSpeed={70}
+                multiTextLoop
+              />
+            </div>
+            <div className="hero__title-4">
+              <TypeWriterEffect
+                textStyle={{
+                  fontFamily: 'Montserrat',
+                  color: '#f0d2d2',
+                  fontWeight: 700,
+                  fontSize: '3.2rem',
+                }}
+                startDelay={0}
+                cursorColor="#f0d2d2"
+                multiText={[
+                  'I build things for the web',
+                  'Full Stack Developer',
+                ]}
+                multiTextDelay={3000}
+                typeSpeed={70}
+                multiTextLoop
+              />
+            </div>
+
+          </div>
+        )
+        : (
+          <div>
+            <div className="hero__title-3">
+              <TypeWriterEffect
+                textStyle={{
+                  fontFamily: 'Montserrat',
+                  color: '#002245',
+                  fontWeight: 700,
+                  fontSize: '6rem',
+                }}
+                startDelay={0}
+                cursorColor="#002245"
+                multiText={[
+                  'I build things for the web',
+                  'Full Stack Developer',
+                ]}
+                multiTextDelay={3000}
+                typeSpeed={70}
+                multiTextLoop
+              />
+            </div>
+            <div className="hero__title-4">
+              <TypeWriterEffect
+                textStyle={{
+                  fontFamily: 'Montserrat',
+                  color: '#002245',
+                  fontWeight: 700,
+                  fontSize: '3.2rem',
+                }}
+                startDelay={0}
+                cursorColor="#002245"
+                multiText={[
+                  'I build things for the web',
+                  'Full Stack Developer',
+                ]}
+                multiTextDelay={3000}
+                typeSpeed={70}
+                multiTextLoop
+              />
+            </div>
+          </div>
+        )}
+      <p
+        className={
+        darkMode
+          ? 'hero__description'
+          : 'hero__description hero__description-light'
+      }
+      >
+        I can help you build a product ,
+        feature or website Look through some of my work and experience! If
+        you like what you see and have a project you need coded, don’t
+        hestiate to contact me.
+      </p>
+      <button className="btn" type="button">LET’S CONNECT</button>
+    </motion.section>
+  );
+};
 
 export default Hero;
